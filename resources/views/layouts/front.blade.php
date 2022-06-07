8d1a49a645acba88253f8a5a47557466144b2660
smartwaypharma <harshad@smartwaypharma.co.uk>
2022-06-07 13:18:01 +0100

    Google Site Verification code updated

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ @csrf_token() }}">
-  <meta name="google-site-verification" content="a11LvvvDD3AijXGSlXvBW8Zx8-GhsVyl5izGywNqYdw" />
+  <meta name="google-site-verification" content="Oamekb-kg2TL0a1zpQHfaWkAmRl2kiF9r479bEKHZpk" />
   @yield('meta')
   <link rel="icon" href="{{ asset('image/favicon.png') }}" sizes="16x16" type="image/png">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
